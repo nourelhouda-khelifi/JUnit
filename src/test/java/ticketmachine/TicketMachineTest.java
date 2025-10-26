2161bc5e7142853b8c183d772b2874a250a4da07
nourelhouda-khelifi <123408799+nourelhouda-khelifi@users.noreply.github.com>
2025-10-26 17:59:41 +0100

    create test for S1-->S10 scenario

--- src/test/java/ticketmachine/TicketMachineTest.java
+++ src/test/java/ticketmachine/TicketMachineTest.java
@@ -33,4 +33,68 @@ class TicketMachineTest {
 		assertEquals(10 + 20, machine.getBalance(), "La balance n'est pas correctement mise à jour");
 	}
 
-}
+	@Test
+	// S3 : on n’imprime pas leticket si le montant inséré est insuffisant
+	void ticketNotPrintedWhenBalanceIsInsufficient() {
+		machine.insertMoney(PRICE - 1);
+		assertFalse( machine.printTicket(), "le montant inséré est suffisant");
+	}
+
+	@Test
+	// S4 : on imprime si le montant inséré est suffisant
+	void ticketPrintedWhenBalanceIsSufficient() {
+		machine.insertMoney(PRICE);
+		assertTrue( machine.printTicket(), "le montant inséré est insuffisant");
+	}
+
+	@Test
+	// S5 : la balance est décrémentée du prix du ticket après impression
+	void balanceDecrementedAfterPrintingTicket() {
+		machine.insertMoney(PRICE + 1); 
+		machine.printTicket(); 
+		assertEquals(1, machine.getBalance(), "la balance n'est pas correctement décrémentée");
+	}
+
+	@Test
+	// S6 : le montant collecté est mis à jour quand on imprime un ticket (pas avant)
+	void collectedAmountIsUpdatedAfterPrintingTicket() {
+		machine.insertMoney(PRICE + 1);
+		machine.printTicket();
+		assertEquals(PRICE, machine.getTotal(), "le montant collecté n'est pas correctement mis à jour");
+	}
+
+	@Test
+	// S7 : refund()rend correctement la monnaie
+	void refundReturnsCorrectBalance() {
+		machine.insertMoney(PRICE + 100);
+		int refund = machine.refund();
+		machine.printTicket();
+		assertEquals(100, refund, "refund() devrait rendre tout l'argent inséré");
+	}
+
+	@Test
+	// S8 : après refund() la balance est remise à zéro
+	void balanceIsZeroAfterRefund() {
+		machine.insertMoney(PRICE + 100);
+		machine.printTicket();
+		machine.refund();
+		assertEquals(0, machine.getBalance(), "la balance devrait être remise à zéro après refund()");
+	}
+
+	@Test
+	// S9 : on ne peut pas insérer un montant négatif
+	void cannotInsertNegativeAmount() {
+		assertThrows(IllegalArgumentException.class, () -> machine.insertMoney(-10),
+                "L'insertion d'un montant négatif devrait provoquer une exception");
+	}
+
+	@Test
+	// S10 :on ne peut pas créer de machine qui délivre des tickets dont le prix est négatif
+	void cannotCreateMachineWithNegativePrice() {
+		assertThrows(IllegalArgumentException.class, () -> new TicketMachine(-1),
+                "La création d'une machine avec un prix négatif devrait provoquer une exception");
+	}
+	}
+
+
+
